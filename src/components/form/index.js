1c9dc1ea4dd57c3f3a5edb8c1587f582d68af359
alexander.shugaev <alexwinner2402@gmail.com>
2018-12-11 14:40:43 +0200

    initial form component commit

---
+++ src/components/form/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Form extends Component {
+    render() {
+        return (
+            <div>
+                privetik
+            </div>
+        );
+    }
+}
+
+Form.propTypes = {};
+
+export default Form;
