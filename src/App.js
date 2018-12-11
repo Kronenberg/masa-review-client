2051c2c56fc74fa4eb90fdb8efe97b235ff8c987
Bixxard <mixa-1997@tut.by>
2018-12-11 18:05:02 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Link  } from 'react-router-dom';
+
+//COMPONENTS
+import Home from './components/home';
+import News from './components/news';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <li>
+            <Link to="/news">NEWS</Link>
+          </li>
+          <Route path="/" exact component={Home}/>
+          <Route path="/news" component={News}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
-
 export default App;
